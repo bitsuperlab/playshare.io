18eec75feda05f12ac29330c53add8af6b05fec7
HackFisher <hackfisher@gmail.com>
2014-11-05 15:16:50 +0800

    consensus

---
+++ site/consensus.php
@@ -0,0 +1,93 @@
+<?php include('head.php'); ?>
+<?php include('menu-page-dark.php'); ?>
+
+	<div id="page" class="page">
+
+        <div class="header-page padd-y-75 marg-top-100 bg-fs-clr">
+        
+            <div class="title-page padd-x-25 wh-clr">Allocation For BitShares PLAY</div>
+            
+        </div>
+                    
+        <div class="post-wrap padd-y-50 boxed">
+                            
+            <article class="row">
+            
+                <div class="post-content padd-x-25">
+                
+                	<div class="row padd-bott-25">
+                    
+                        <h3>Bitshares PLAY</h3>
+    
+                            Bitshares PLAY is positioned to be a self-marketable and self-profitable DAC that would be able to blend into existing network systems to be a sustainable platform that encourages game assets and value-adding services. PLS is the main token of the system, the inital total supply of PLS will be 2 billion, with 100% as maximum delegate pay.
+                    
+                    </div>
+                    
+                    <div class="clear marg-bott-25"></div>
+                    
+                	<div class="col-1-2 padd-right-25 padd-bott-25">
+                    
+                        <h3>BTS</h3>
+                        
+                            The block to take the snapshot on BTS undecided, BTS being the SuperDAC after the merger. The number of the block will be published after BTS being launched.
+                    
+                    </div>
+                    
+                	<div class="col-1-2 padd-bott-25">
+                    
+                        <h3>AGS</h3>
+                        
+                            From 5th November 2014 snapshot, no action required other than import of private keys.
+                        
+                    </div>
+                    
+                    <div class="clear marg-bott-25"></div>
+                    
+                	<div class="col-1-2 padd-right-25 padd-bott-25">
+                    
+                        <h3>PTS</h3>
+                        
+                            The block to take the snapshot on PTS is on November 5, 2014. This is also the anniversary snapshot for both BTS and PLAY.
+                    
+                    </div>
+                    
+                	<div class="col-1-2 padd-bott-25">
+                    
+                        <h3>Crowd Saling</h3>
+                        
+                            It will be an AGS-like pre-sale, raise through Bitcoin. The start time and details will be announced later, please stay tuned.
+                        
+                    </div>
+                    
+                    <div class="clear marg-bott-25"></div>
+                    
+                	<div class="col-1-2 padd-right-25 padd-bott-25">
+                    
+                        <h3>Development and Marketing</h3>
+                        
+                            Long-term development, including a part to cover costs in pre-launch period.
+                    
+                    </div>
+                    
+                	<div class="col-1-2 padd-bott-25">
+                    
+                        <h3>Reserved Funds</h3>
+                        
+                            Reserved fund is for specific purposes including Faucet, Business Development, Game Incubation, long-term ecosystem building.</br>
+                        
+                            Reserved Fund will be locked on initial launch, and will be linear unlocked monthly after launch in 1 year. 
+                    
+                    </div>
+                    
+                    <div class="clear marg-bott-25"></div>
+                    
+                </div>
+                
+            </article>
+                                                
+        </div>
+                        
+    </div>
+    
+<?php include('copy.php'); ?>
+<?php include('foot.php'); ?>
