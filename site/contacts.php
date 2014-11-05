c7dd6488550c5ba89b4ce078b244b9ef35b4761a
HackFisher <hackfisher@gmail.com>
2014-11-05 11:02:55 +0800

    status

--- site/contacts.php
+++ site/contacts.php
@@ -1,7 +1,7 @@
 <?php include('head.php'); ?>
 <?php include('menu-page-dark.php'); ?>
 
-<?php include('section/contacts.php'); ?>
+<?php include('sections/form.php'); ?>
 
 <?php include('copy.php'); ?>
 <?php include('foot.php'); ?>
