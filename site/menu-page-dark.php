1f78ca4e98015bbcf1b55c2bad36d5d695a181de
Alex Chien <alex.chien@koocaa.com>
2014-11-07 17:26:03 +0800

    #1 simple contact email

--- site/menu-page-dark.php
+++ site/menu-page-dark.php
@@ -4,22 +4,22 @@
     <div id="logo-dark" class="logo"><a href="index.php"><img src="img/logo-dark.png" /></a></div>
 
     <div class="main-menu">
-            
+
         <ul>
             <li class="menu-item-parent"><a href="index.php">home</a>
-            	<ul class="sub-menu transit">
-                	<!--<li><a href="index-video.php">home video</a></li>-->
+              <ul class="sub-menu transit">
+                  <!--<li><a href="index-video.php">home video</a></li>-->
                     <li><a href="vision.php">Vision</a></li>
                     <li><a href="faqs.php">FAQs</a></li>
                 </ul>
             </li>
             <li class="menu-item-parent"><a href="page-full.php">Resources</a>
-            	<ul class="sub-menu transit">
-					<li><a href="about-page.php">about</a></li>
-            		<li><a href="consensus.php">consensus</a></li>
-            		<li><a href="contacts.php">contacts</a></li>
-                	<!--<li><a href="page-side-left.php">sidebar left</a></li>-->
-                	<!--<li><a href="page-side-right.php">sidebar right</a></li>-->
+              <ul class="sub-menu transit">
+          <li><a href="about-page.php">about</a></li>
+                <li><a href="consensus.php">consensus</a></li>
+                <li><a href="contacts.php">contacts</a></li>
+                  <!--<li><a href="page-side-left.php">sidebar left</a></li>-->
+                  <!--<li><a href="page-side-right.php">sidebar right</a></li>-->
                 </ul>
             </li>
             <li><a href="blogs.php">blog</a></li>
@@ -27,45 +27,45 @@
             <li><a href="http://bitsharesplayfoundation.org">foundation</a></li>
             <li><a href="http://bitshares.org">bitshares</a></li>
         </ul>
-        
+
     </div>
-    
+
     <div id="quick-icons">
-    	
+
         <ul class="info-menu">
-            <li><a title="support" href="mailto:contact@bitsuperlab.com"><i class="icon-mail"></i></a></li>
+            <li><a title="support" href="mailto:contact@bitsuperlab.com" data-curtain="false"><i class="icon-mail"></i></a></li>
             <li><a title="follow us" href="http://www.bitsuperlab.com"><i class="icon-heart"></i></a></li>
         </ul>
-        
+
         <ul class="switch-menu">
             <li class="open-menu"><i class="icon-menu"></i></li>
         </ul>
-        
+
         <div class="clear"></div>
-        
-	</div>
-    
+
+  </div>
+
     <div class="clear"></div>
-    
+
 </header>
 
 <div id="mobile-menu">
-            
+
     <ul>
             <li class="menu-item-parent"><a href="index.php">home</a>
-            	<ul class="sub-menu transit">
-                	<!--<li><a href="index-video.php">home video</a></li>-->
+              <ul class="sub-menu transit">
+                  <!--<li><a href="index-video.php">home video</a></li>-->
                     <li><a href="vision.php">Vision</a></li>
                     <li><a href="faqs.php">FAQs</a></li>
                 </ul>
             </li>
             <li class="menu-item-parent"><a href="page-full.php">Resources</a>
-            	<ul class="sub-menu transit">
-					<li><a href="about-page.php">about</a></li>
-            		<li><a href="consensus.php">consensus</a></li>
-            		<li><a href="contacts.php">contacts</a></li>
-                	<!--<li><a href="page-side-left.php">sidebar left</a></li>-->
-                	<!--<li><a href="page-side-right.php">sidebar right</a></li>-->
+              <ul class="sub-menu transit">
+          <li><a href="about-page.php">about</a></li>
+                <li><a href="consensus.php">consensus</a></li>
+                <li><a href="contacts.php">contacts</a></li>
+                  <!--<li><a href="page-side-left.php">sidebar left</a></li>-->
+                  <!--<li><a href="page-side-right.php">sidebar right</a></li>-->
                 </ul>
             </li>
             <li><a href="blogs.php">blog</a></li>
@@ -73,5 +73,5 @@
             <li><a href="http://bitsharesplayfoundation.org">foundation</a></li>
             <li><a href="http://bitshares.org">bitshares</a></li>
     </ul>
-        
+
 </div>
